1a80d3fcb2e519529cea4c481418970277d54991
Akuna Joshua <akunajoshua@gmail.com>
2023-10-27 19:29:49 +0100

    3. Status of your API : files added

---
+++ api/v1/views/index.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Handles routes requests
+'''
+
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route('/status', methods=['GET'])
+def status():
+    '''Returns a JSON object for status {"status": "OK"}
+    '''
+    return jsonify({"status": "OK"})
