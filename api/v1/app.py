9e2582066dddc8509bce740d24d00247ed3aeec6
Akuna Joshua <akunajoshua@gmail.com>
2023-10-27 20:16:59 +0100

    api/v1/app.py file updated for host and port

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,6 +7,7 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
@@ -20,4 +21,6 @@ def teardown(self):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', 5000)
+    app.run(host=host, port=port, threaded=True)
