21727fc502fd17df854c355554bf8edeb27f7909
Akuna Joshua <akunajoshua@gmail.com>
2023-10-27 20:34:51 +0100

    task 5. Not found: files commit

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,13 +4,21 @@
 '''
 
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
+
+
+@app.errorhandler(404)
+def not_found(e):
+    '''The endpoint handles 404 errors by returning a JSON
+        object {'error': 'Not found'}
+    '''
+    return jsonify({'error': 'Not found'}), 404
 
 
 @app.teardown_appcontext
