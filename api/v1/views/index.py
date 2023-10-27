7fb224d054b1a0de1e85e71c9d1d330cbbcfae34
Akuna Joshua <akunajoshua@gmail.com>
2023-10-27 20:05:49 +0100

    app.py and index.py files updated

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,7 +6,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     '''Returns a JSON object for status {"status": "OK"}
     '''
