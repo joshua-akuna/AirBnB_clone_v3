7abaa38257ee6890c43f5dbe1a7de2ebadbfefa7
Akuna Joshua <akunajoshua@gmail.com>
2023-10-28 11:31:50 +0100

    task 12. HTTP access control : implementations added

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,11 +5,14 @@
 
 
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
 
